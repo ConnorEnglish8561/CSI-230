b7dfe05880a47a815d1a09cf42f383e900bab1f4
ConnorEnglish8561 <148250198+ConnorEnglish8561@users.noreply.github.com>
2025-09-09 11:55:03 -0400

    uploaded process management files

---
+++ Process Management 1/problem3.ps1
@@ -0,0 +1,3 @@
+﻿$path = "$env:USERPROFILE\Downloads\StoppedServices.csv"
+Get-Service | Where-Object { $_.Status -eq "Stopped" } | Sort-Object | Export-Csv -Path $path -NoTypeInformation
+
---
+++ Process Management 1/problem4.ps1
@@ -0,0 +1,7 @@
+﻿if (Get-Process -Name "chrome"){
+    Stop-Process -Name "chrome"
+}
+else{
+   Start-Process -FilePath "chrome.exe" -ArgumentList "https://www.champlain.edu/"
+}
+
