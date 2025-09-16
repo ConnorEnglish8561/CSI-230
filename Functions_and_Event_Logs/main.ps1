0da99a1bb0dc6946916dec216e077e8a4c9f71b3
ConnorEnglish8561 <148250198+ConnorEnglish8561@users.noreply.github.com>
2025-09-16 12:00:39 -0400

    Add files via upload

---
+++ Functions_and_Event_Logs/main.ps1
@@ -0,0 +1,11 @@
+﻿.(Join-Path $PSScriptRoot  -ChildPath "functions.ps1")
+clear
+
+$logTable = GetLogInOut(14)
+$logTable
+
+$shutDownTable = GetStartShutdown 25 "Shutdown"
+$shutDownTable
+
+$startUpTable = GetStartShutdown 25 "Startup"
+$startUpTable
